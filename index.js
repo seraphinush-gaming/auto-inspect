75c22b050e41550e0cdec4e68c5bcce3c0a7547d
seraph <30970630+seraphinush-gaming@users.noreply.github.com>
2018-03-20 21:48:48 +0900

    1.01 r:00

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
-// OPCODE REQUIRED :
-// - C_REQUEST_USER_PAPERDOLL_INFO
-// - S_ASK_INTERACTIVE
-
-// Version 1.00 r:01
+// Version 1.01 r:00
 
 module.exports = function AutoInspect(d) {
 
     //code
-    d.hook('S_ANSWER_INTERACTIVE', (e) => { 
+    d.hook('S_ANSWER_INTERACTIVE', (e) => {
         d.toServer('C_REQUEST_USER_PAPERDOLL_INFO', { name: e.name })
     })
 
