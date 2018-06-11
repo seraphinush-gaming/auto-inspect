47a1da4b520dfb45b81d9290a77f7ae05ece59fc
seraph <30970630+seraphinush-gaming@users.noreply.github.com>
2018-06-11 16:42:25 +0900

    1.01 r:03

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-// Version 1.01 r:02
+// Version 1.01 r:03
 
 module.exports = function AutoInspect(d) {
 
     //code
-    d.hook('S_ANSWER_INTERACTIVE', (e) => {
+    d.hook('S_ANSWER_INTERACTIVE', 2, (e) => {
         d.send('C_REQUEST_USER_PAPERDOLL_INFO', 1, { name: e.name })
     })
 
