bb45a0059ad19ffa4e4b2760072245a8c650361f
seraph <seraphinush@gmail.com>
2021-02-25 02:32:50 +0900

    1.04.08.v103.02

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ class auto_inspect {
   constructor(mod) {
 
     // code
-    mod.hook('S_ANSWER_INTERACTIVE', 2, (e) => {
+    mod.hook('S_ANSWER_INTERACTIVE', mod.majorPatchVersion >= 103 ? 3 : 2, (e) => {
       mod.send('C_REQUEST_USER_PAPERDOLL_INFO', 3, { name: e.name });
     });
 
