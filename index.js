7808211e901f9e435b4ddac94f27062385d6d35d
seraph <seraphinush@gmail.com>
2021-08-31 16:43:02 +0900

    1.04.0d.v109.00

--- index.js
+++ index.js
@@ -4,19 +4,6 @@ class AutoInspect {
 
   constructor(mod) {
 
-    // definition
-    try {
-      mod.dispatch.addDefinition('C_REQUEST_USER_PAPERDOLL_INFO', 4, [
-        ['name', 'refString'],
-        ['serverId', 'uint32'],
-        ['zoom', 'bool'],
-        ['name', 'string']
-      ], false);
-    }
-    catch {
-      mod.error('Failed to add definition.');
-    }
-
     // code
     mod.hook('S_ANSWER_INTERACTIVE', mod.majorPatchVersion >= 103 ? 3 : 2, (e) => {
       mod.send('C_REQUEST_USER_PAPERDOLL_INFO', mod.majorPatchVersion >= 108 ? 4 : 3, {
